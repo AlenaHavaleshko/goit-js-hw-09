7814c0ebcffa58a64cd4ee35d5edad809133cbb8
Alena Havalehko <126282451+AlenaHavaleshko@users.noreply.github.com>
2025-05-01 15:27:12 +0300

    add    base: '/goit-js-hw-09/' to config file

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
+
+
 export default defineConfig(({ command }) => {
   return {
+
+    base: '/goit-js-hw-09/',
+    
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
